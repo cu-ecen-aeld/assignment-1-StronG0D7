1cf3052de748473e947447859ccafba60e6c9d78
StronG0D7 <mape2320@colorado.edu>
2025-01-08 00:18:34 -0600

    Update test_assignment_validate.c

--- test_assignment_validate.c
+++ test_assignment_validate.c
@@ -1,1 +1,14 @@
+#include "unity.h"
+#include <stdbool.h>
 
+
+/**
+* Automated test code 
+*/
+void test_assignment_validate()
+{
+*Test true or false*
+  if f(x)=("statement"), 
+  print("the function should return true");
+else: 
+print("the function  should return false");
